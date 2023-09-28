1b7c590b33078511386867a3db3f92355c36d18f
Tejaswi Gowda <tejaswi@asu.edu>
2023-09-28 12:27:39 -0700

    ac

--- watchSensors/captureDataServer-TempHum/serverwithdb.js
+++ watchSensors/captureDataServer-TempHum/serverwithdb.js
@@ -3,11 +3,13 @@ var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
+var MS = require("mongoskin");
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 1234;
 
 var t, h;
 
+var db = MS.db("mongodb://13.56.213.25:27017/sensorData");
 
 app.get("/", function (req, res) {
     res.redirect("index.html")
@@ -18,7 +20,9 @@ app.get("/sendData", function (req, res) {
   h = req.query.h;
     req.query.time = new Date().getTime();
 
-    res.send("ok");
+    db.collection("data").insert(req.query, function(result, err){
+      res.send("1");
+    });
 });
 
 
